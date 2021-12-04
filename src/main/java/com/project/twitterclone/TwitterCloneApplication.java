4481831bfcb3d8938171205bb6c94ed8d35f917f
alper.kurtoglu <alper.kurtoglu@tubitak.gov.tr>
2021-12-04 18:15:10 +0300

    TWT-2 openapi-swagger added.

--- src/main/java/com/project/twitterclone/TwitterCloneApplication.java
+++ src/main/java/com/project/twitterclone/TwitterCloneApplication.java
@@ -2,12 +2,13 @@ package com.project.twitterclone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class TwitterCloneApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TwitterCloneApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterCloneApplication.class, args);
+    }
 
 }
