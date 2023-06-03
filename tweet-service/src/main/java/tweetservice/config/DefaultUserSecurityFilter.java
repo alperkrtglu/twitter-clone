3561c24fc950294988e47111bf00e8d66f336f7d
alper.kurtoglu <alper.kurtoglu@tubitak.gov.tr>
2023-06-03 17:43:08 +0300

    TWT-13 Added Spring Security to tweet-service and set up mock SecurityContextHolder

---
+++ tweet-service/src/main/java/tweetservice/config/DefaultUserSecurityFilter.java
@@ -0,0 +1,30 @@
+package tweetservice.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+public class DefaultUserSecurityFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        Authentication authentication =
+                new UsernamePasswordAuthenticationToken("admin", "123", List.of(new SimpleGrantedAuthority("ROLE_ADMIN")));
+
+        SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+        securityContext.setAuthentication(authentication);
+        SecurityContextHolder.setContext(securityContext);
+
+        filterChain.doFilter(request, response);
+    }
+}
