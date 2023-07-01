3d87d2e3e79fe1fb80492df90851940b41c6c7aa
alper.kurtoglu <alper.kurtoglu@tubitak.gov.tr>
2023-07-01 13:17:19 +0300

    TWT-24 Added Keycloak configuration for api-gateway and tweet-sercice

--- tweet-service/src/main/java/tweetservice/config/SecurityConfig.java
+++ tweet-service/src/main/java/tweetservice/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import tweetservice.security.MockAuthenticationFilter;
@@ -14,14 +15,18 @@ public class SecurityConfig {
 
     @Bean
     protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .addFilterAfter(mockAuthenticationFilter(), BasicAuthenticationFilter.class)
-                .csrf().disable()
-                .build();
+        http.csrf(AbstractHttpConfigurer::disable);
+        http
+                .authorizeHttpRequests()
+                .anyRequest().authenticated().and()
+                .oauth2ResourceServer()
+                .jwt();
+        //    .addFilterAfter(mockAuthenticationFilter(), BasicAuthenticationFilter.class)
+        return http.build();
     }
 
-    @Bean
-    public MockAuthenticationFilter mockAuthenticationFilter() {
-        return new MockAuthenticationFilter();
-    }
+//    @Bean
+//    public MockAuthenticationFilter mockAuthenticationFilter() {
+//        return new MockAuthenticationFilter();
+//    }
 }
